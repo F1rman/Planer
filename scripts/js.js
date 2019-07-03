c12381e39df234d0d1da42323987c2f19679654b
F1rman <ihorkharchyshyn@gmail.com>
2019-07-03 15:25:20 +0300

    now i need some functional

--- scripts/js.js
+++ scripts/js.js
@@ -3,6 +3,7 @@ var storage = window.localStorage;
 // storage.setItem('key',val );
 // storage.setItem('passlvl',16);
 
+
 var d = new Date();
 var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
@@ -18,15 +19,34 @@ app.run(($rootScope) => {
 
 
 app.controller('Main', function($scope) {
+  var task_progress = 25;
+  var task_color = 'red lighten-3';
   $scope.toDos = [{
-      'description': 'Wash the car',
-      'dueBy': Date.now()
+      'title': 'Wash the car',
+      'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'dueBy': Date.now(),
+      'progress' : task_progress,
+      'color': task_color
     },
     {
-      'description': 'Try out Angular.js on CodePen',
-      'dueBy': Date.now()
+      'title': 'Try out Angular.js on CodePen',
+      'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'dueBy': Date.now(),
+      'progress' : task_progress+10,
+      'color': 'blue'
     }
   ];
+  console.log($scope.toDos);
+    var todo = JSON.stringify($scope.toDos);
+if (storage.getItem('tasks') == null) {
+ $scope.toDos = JSON.parse(storage.getItem('tasks'))
+}
+
+
+
+  console.log(todo);
+  // storage.setItem('tasks',todo);
+  console.log(JSON.parse(storage.getItem('tasks')));
 
   $scope.addToDo = function() {
     var formattedDate = Date.parse($scope.formDueDate);
